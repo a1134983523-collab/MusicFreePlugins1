1e9b1f724c1271a7238deaa531a65d913971c50c
a1134983523-collab <a1134983523@gmail.com>
2026-08-10 04:02:15 +0800

    更新 index.js

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
+/**
+ * MusicFree 0.6.2 测试插件
+ * 用于验证“从网络安装插件”是否正常
+ */
+
 module.exports = {
-  platform: '测试插件',
+  platform: '我的音乐测试源',
+
   version: '1.0.0',
+
   author: 'a1134983523-collab',
 
-  async search(query, page) {
+  description: 'MusicFree 0.6.2 插件安装测试',
+
+  srcUrl:
+    'https://github.com/a1134983523-collab/MusicFreePlugins1/raw/main/index.js',
+
+  async searchMusic(keyword, page) {
+    return [];
+  },
+
+  async getMediaSource(musicItem) {
     return {
-      isEnd: true,
-      data: []
+      url: '',
     };
-  }
+  },
 };
